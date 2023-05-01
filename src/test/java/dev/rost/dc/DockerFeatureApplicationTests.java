828381f90594f2e7add1df4dc80c23a7ce490de1
Rostislav <zhistovskiy@vsk.ru>
2023-05-01 09:28:06 +0300

    feat(master): docker compose new feature
    * Init commit

---
+++ src/test/java/dev/rost/dc/DockerFeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rost.dc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerFeatureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
